fbee76968f8c3b7c6d643248918b64f3f7db48d2
Mick Luypaerts <mickluypaerts@gmail.com>
2021-06-09 13:27:54 +0200

    Added the mandelbrot function to calculate the number of iterations needed to reach a modulus greater than 2

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/cmplx"
+)
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	var c complex128
+	var i float64
+	var y float64
+	for i = -10; i < 10; i+= 5 {
+		for y = -10; y < 10; y+= 5 {
+			c = complex(i / 10, y / 10)
+			fmt.Println(c, mandelbrot(c))
+		}
+	}
+}
+
+func mandelbrot(c complex128) float64 {
+	var z complex128
+	var n complex128
+	
+	for cmplx.Abs(z) <= 2 && real(n) < 80 {
+		z = z * z + c
+		n += 1
+	}
+	return real(n)
 }
